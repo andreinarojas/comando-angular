56fd5f2eb3f7732f461ac61ee57f0b327c9af4f9
andreinarojas <77947483+andreinarojas@users.noreply.github.com>
2021-04-10 20:07:42 -0300

    componente ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'proyecto-andre';
-}
+  title = 'Menu';
+  title2 = 'hola soy el segundo titulo';
+  foods= ['Pollo', 'Pescado', 'Carne', 'Pastas'];
+  foodName= '';
+
+  addFood (){
+    this.foods.push(this.foodName);
+    this.foodName = '';
+  }
+  deleteFood () {
+    this.foods.pop()
+   // this.food=[]
+  }
+  }
